62ef8770e56b9c977c6e3d294021530661e6e2e0
foreverfl <forever_fl@naver.com>
2022-04-22 19:33:34 +0900

    220422 19:30

---
+++ 06_AOP(1) Reflection/src/test/java/com/example/AOP/reflection/Reflection.java
@@ -0,0 +1,30 @@
+package com.example.AOP.reflection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.reflect.Method;
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+public class Reflection {
+	@Test
+	public void invokeMethod() throws Exception {
+		String name = "Spring";
+
+		assertEquals(name.length(), 6);
+
+		Method lengthMethod = String.class.getMethod("length");
+		assertEquals((Integer) lengthMethod.invoke(name), 6);
+
+		assertEquals(name.charAt(0), 'S');
+
+		Method charAtMethod = String.class.getMethod("charAt", int.class);
+		assertEquals((Character) charAtMethod.invoke(name, 0), 'S');
+	}
+
+	@Test
+	public void createObject() throws Exception {
+		Date now = (Date) Class.forName("java.util.Date").getDeclaredConstructor().newInstance();
+	}
+}
