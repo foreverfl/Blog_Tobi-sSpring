42fd163e5242d1f0023c8f30917a9d277b73a296
foreverfl <forever_fl@naver.com>
2022-04-23 12:30:19 +0900

    220423 12:30

--- 06_AOP(2) Dynamic Proxy/src/test/java/com/example/AOP/dynamicProxy/DynamicProxyTest.java
+++ 06_AOP(2) Dynamic Proxy/src/test/java/com/example/AOP/dynamicProxy/DynamicProxyTest.java
@@ -12,16 +12,43 @@ public class DynamicProxyTest {
 	@Test
 	public void simpleProxy() {
 		Hello hello = new HelloTarget();
-		assertEquals(hello.sayHello("Toby"), "Hello Toby");
-		assertEquals(hello.sayHi("Toby"), "Hi Toby");
-		assertEquals(hello.sayThankYou("Toby"), "Thank You Toby");
+		assertEquals(hello.sayHello("Taro"), "Hello Taro");
+		assertEquals(hello.sayHi("Taro"), "Hi Taro");
+		assertEquals(hello.sayThankYou("Taro"), "Thank You Taro");
+
+		Hello hello_uppercase = new HelloUppercase(new HelloTarget());
+		assertEquals(hello_uppercase.sayHello("Taro"), "HELLO TARO");
+		assertEquals(hello_uppercase.sayHi("Taro"), "HI TARO");
+		assertEquals(hello_uppercase.sayThankYou("Taro"), "THANK YOU TARO");
 
 		Hello proxiedHello = (Hello) Proxy.newProxyInstance(getClass().getClassLoader(), new Class[] { Hello.class },
 				new UppercaseHandler(new HelloTarget()));
 
-		assertEquals(proxiedHello.sayHello("Toby"), "HELLO TOBY");
-		assertEquals(proxiedHello.sayHi("Toby"), "HI TOBY");
-		assertEquals(proxiedHello.sayThankYou("Toby"), "THANK YOU TOBY");
+		assertEquals(proxiedHello.sayHello("Taro"), "HELLO TARO");
+		assertEquals(proxiedHello.sayHi("Taro"), "HI TARO");
+		assertEquals(proxiedHello.sayThankYou("Taro"), "THANK YOU TARO");
+	}
+
+	static interface Hello {
+		String sayHello(String name);
+
+		String sayHi(String name);
+
+		String sayThankYou(String name);
+	}
+
+	static class HelloTarget implements Hello {
+		public String sayHello(String name) {
+			return "Hello " + name;
+		}
+
+		public String sayHi(String name) {
+			return "Hi " + name;
+		}
+
+		public String sayThankYou(String name) {
+			return "Thank You " + name;
+		}
 	}
 
 	static class HelloUppercase implements Hello {
@@ -62,25 +89,4 @@ public class DynamicProxyTest {
 		}
 	}
 
-	static interface Hello {
-		String sayHello(String name);
-
-		String sayHi(String name);
-
-		String sayThankYou(String name);
-	}
-
-	static class HelloTarget implements Hello {
-		public String sayHello(String name) {
-			return "Hello " + name;
-		}
-
-		public String sayHi(String name) {
-			return "Hi " + name;
-		}
-
-		public String sayThankYou(String name) {
-			return "Thank You " + name;
-		}
-	}
 }
